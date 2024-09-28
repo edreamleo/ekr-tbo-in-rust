4eb05720608c52d506c2425534a39d47c591f1df
Edward K. Ream <edreamleo@gmail.com>
2024-09-28 18:10:03 -0500

    Simplify. Use zero fill

--- src/tbo.rs
+++ src/tbo.rs
@@ -18,58 +18,48 @@ fn fmt_ms(t: u128) -> String {
     //! Convert a time in microsecond to fractional millisecons.
     let ms = t / 1000;
     let micro = (t % 1000) / 10;
-    return f!("{ms}.{micro:2}");
+    return f!("{ms}.{micro:02}");
 }
 
 pub fn entry() {
-
+    // Set file name.
     let file_path = "C:\\Repos\\leo-editor\\leo\\core\\leoApp.py";
     let short_file_name = "leoApp.py";
-
-    // Read leoApp.py.
+    // Read.
     let t1 = Instant::now();
     let contents = fs::read_to_string(file_path).expect("Can not read file");
-    let read_time = t1.elapsed().as_micros();
-
+    let read_time = fmt_ms(t1.elapsed().as_micros());
     // Tokenize.
     let t2 = Instant::now();
     let tokens = lex(&contents, Mode::Module)
         .map(|tok| tok.expect("Failed to lex"))
         .collect::<Vec<_>>();
-    let tokenize_time = t2.elapsed().as_micros();
-
-    // :? is debugging format.
+    let tokenize_time = fmt_ms(t2.elapsed().as_micros());
     let t3 = Instant::now();
+    // Loop on tokens.
     let mut n_tokens: usize = 0;
     for (token, range) in tokens {
         // Range is a TextRange.
         n_tokens += 1;
         // To do: Find gaps in the ranges.
-
-        // These conversions are fast!
         let start_i = usize::from(range.start());
         let end_i = usize::from(range.end());
-        
         if false {
             if n_tokens < 20 {
                 println_f!("{start_i:>3}..{end_i:3} token: {token:?}");
             }
         }
     }
-    let loop_time = t3.elapsed().as_micros();
-    let total_time = t1.elapsed().as_micros();
+    let loop_time = fmt_ms(t3.elapsed().as_micros());
+    let total_time = fmt_ms(t1.elapsed().as_micros());
 
     // Sign on.
     println!("");
     println_f!("     tbo: {short_file_name}: {n_tokens} tokens\n");
     // Print stats.
-    let read_ms = fmt_ms(read_time);
-    println_f!("    read: {read_ms:>5} ms");
-    let tokenize_ms = fmt_ms(tokenize_time);
-    println_f!("tokenize: {tokenize_ms:>5} ms");
-    let loop_ms = fmt_ms(loop_time);
-    let total_ms = fmt_ms(total_time);
-    println_f!("    loop: {loop_ms:>5} ms");
-    println_f!("   total: {total_ms:>5} ms");
+    println_f!("    read: {read_time:>5} ms");
+    println_f!("tokenize: {tokenize_time:>5} ms");
+    println_f!("    loop: {loop_time:>5} ms");
+    println_f!("   total: {total_time:>5} ms");
 }
 //@-leo
