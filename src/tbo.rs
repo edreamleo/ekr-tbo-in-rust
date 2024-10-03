6744a8859fbe3644b9d0d424b69c99f71f25a16d
Edward K. Ream <edreamleo@gmail.com>
2024-10-03 10:03:49 -0500

    Add better tests. Mysteries & frustrations remain

--- src/tbo.rs
+++ src/tbo.rs
@@ -18,25 +18,79 @@
 // use std::path;
 
 //@+others
-//@+node:ekr.20241001093308.1: ** pub fn entry (test)
+//@+node:ekr.20241003093554.1: **  pub fn entry
 pub fn entry() {
+    if true {
+        test();
+    } else {
+        main();
+    }
+}
+//@+node:ekr.20241003094145.1: **  struct TestTok
+#[derive(Clone)]
+#[derive(Debug)]
+// Weird
+#[allow(dead_code)]
+pub struct TestTok {
+    value: i32,
+}
+//@+node:ekr.20241003093722.1: ** fn main
+pub fn main() {
+    println!("main: not ready yet");
+}
+//@+node:ekr.20241001093308.1: ** fn test
+fn test() {
+    test_vec();
+    test_struct();
+}
+//@+node:ekr.20241003094218.2: ** fn test_struct
+fn test_struct() {
     // Test code for Vec.
-    let mut v: Vec<i32> = Vec::new();
-    push(&mut v, 1);
-    push(&mut v, 2);
+    let mut v: Vec<TestTok> = Vec::new();
+    push_struct(&mut v, 1);
+    push_struct(&mut v, 2);
     for i in [3, 4, 6] {
-        push(&mut v, i);
+        push_struct(&mut v, i);
     }
     let mut i = 7;
     while i < 10 {
-        push(&mut v, i);
+        push_struct(&mut v, i);
+        i += 1
+    }
+    // This fails
+    // let mut tok = v[0];
+    // tok.value = 666;
+    // println!("{tok:?}");
+    println!("");
+    for z in v {
+        println!("{z:?}");
+    }
+}
+
+fn push_struct(v: &mut Vec<TestTok>, val: i32) {
+    let mut tok = TestTok{value: 0};
+    tok.value = val;  // To test mutability.
+    v.push(tok);
+}
+//@+node:ekr.20241003094218.1: ** fn test_vec & push_vec
+fn test_vec() {
+    // Test code for Vec.
+    let mut v: Vec<i32> = Vec::new();
+    push_vec(&mut v, 1);
+    push_vec(&mut v, 2);
+    for i in [3, 4, 6] {
+        push_vec(&mut v, i);
+    }
+    let mut i = 7;
+    while i < 10 {
+        push_vec(&mut v, i);
         i += 1
     }
     println!("");
     println!("v: {v:?}");
 }
 
-fn push(v: &mut Vec<i32>, val: i32) {
+fn push_vec(v: &mut Vec<i32>, val: i32) {
     v.push(val);
 }
 //@-others
