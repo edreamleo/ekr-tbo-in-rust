ab218aacfc39287dd8d111908fa476f7eded46fa
Edward K. Ream <edreamleo@gmail.com>
2024-10-07 01:13:24 -0500

    Add test folder and test1.py

---
+++ test/test1.py
@@ -0,0 +1,10 @@
+#@+leo-ver=5-thin
+#@+node:ekr.20241007005233.1: * @file test/test1.py
+#@@language python
+
+class TestClass:
+
+    def test_ops(self, arg1, *args, **kwargs):
+        a = 2
+        print(a)
+#@-leo
